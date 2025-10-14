1c4bfd920d057730b5277f829f1e906596013740
pyg1334 <pyg1334@gmail.com>
2025-10-14 17:00:43 +0900

    Spring Cloud Bus를 이용한 설정 자동 새로고침 구축

--- msa/user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ msa/user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -1,4 +1,6 @@
 package com.example.user_service.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,10 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RefreshScope // 스프링에게 "이 컴포넌트는 애플리케이션 재시작 없이 설정을 새로고침 할 수 있다"고 알려줌
 public class UserController {
+
+    // Config 서버에서 값을 읽어옴
+    @Value("${test.message}")
+    private String message;
 
     @GetMapping("/{id}")
     public String getUserById(@PathVariable String id) {
         return "Response from User-Service for user ID: " + id;
+    }
+
+    @GetMapping("/message")
+    public String getMessage() {
+        return message;
     }
 }
