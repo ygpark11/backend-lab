f32de3788bb636e017c31ef03f49f0cd3b3fead0
ygpark11 <pyg1334@gmail.com>
2026-05-12 16:00:44 +0900

    fix: RatingTargetResponse 변수명 수정

--- msa/apps/catalog-service/catalog-service/src/main/java/com/pstracker/catalog_service/scraping/dto/RatingTargetResponse.java
+++ msa/apps/catalog-service/catalog-service/src/main/java/com/pstracker/catalog_service/scraping/dto/RatingTargetResponse.java
@@ -3,5 +3,5 @@ package com.pstracker.catalog_service.scraping.dto;
 public record RatingTargetResponse(
         Long jobId,
         Long gameId,
-        String title
+        String searchTitle
 ) {}
